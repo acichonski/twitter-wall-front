6e92e5ca12e7bf661aa2192b6c903812220c2c53
Adrian Cichoński <acichonski@hotmail.com>
2020-05-17 12:51:51 +0200

    Disable KarmaJS watch test

--- karma.conf.js
+++ karma.conf.js
@@ -23,15 +23,9 @@ module.exports = function (config) {
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
